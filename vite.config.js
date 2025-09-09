58c29186a182bee3133c667ab609449db4d07656
Kyle Johnson <github.com@kyle.engineer>
2025-09-08 20:55:19 -0700

    feat: lazy load plotly charts and add bundle visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ filename: 'stats.html', template: 'treemap', open: false }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
