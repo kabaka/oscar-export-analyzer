fd48b9e8f1f4d0da0a3293b2a2bc0e5142d24617
Kyle Johnson <github.com@kyle.engineer>
2025-09-09 07:07:26 -0700

    feat: add themed plot wrapper

---
+++ src/components/ThemedPlot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+import { useEffectiveDarkMode } from '../hooks/useEffectiveDarkMode';
+import { applyChartTheme } from '../utils/chartTheme';
+
+export default function ThemedPlot({ layout, ...props }) {
+  const isDark = useEffectiveDarkMode();
+  const themedLayout = applyChartTheme(isDark, layout);
+  return (
+    <Plot key={isDark ? 'dark' : 'light'} layout={themedLayout} {...props} />
+  );
+}
